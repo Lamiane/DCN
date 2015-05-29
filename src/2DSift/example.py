6dde34a5429c9f8860d9aa3574a6aea94a9669ba
Agnieszka Pocha <agnieszka.pocha@uj.edu.pl>
2015-05-29 17:02:36 +0200

    played a little, saving for security

--- src/2DSift/example.py
+++ src/2DSift/example.py
@@ -1,10 +1,22 @@
+__author__ = 'agnieszka'
+
 from twodsifts_dataset import *
 from os.path import join
+from pylearn2.utils.serial import save
 
 
 def build_dataset():
     dirpath = '../../data'
     data = TwoDSiftData([join(dirpath, '2RH1_actives_2dfp.dat'),
                          join(dirpath, '2RH1_inactives_2dfp.dat'),
-                         join(dirpath, '2RH1_middle_2dfp.dat')], [[2], [1], [0]])
+                         join(dirpath, '2RH1_middle_2dfp.dat')], [[2], [1], [0]], cv=[5, [0, 1, 2, 3]])
     return data
+
+
+def save_data(path, data_to_save):
+    save(path, data_to_save)
+
+
+def check_files_identity(path1, path2):
+    import filecmp
+    filecmp.cmp(path1, path2)
--- src/2DSift/twodsifts_dataset.py
+++ src/2DSift/twodsifts_dataset.py
@@ -1,22 +1,20 @@
 __author__ = 'igor'
 
+# todo: zoptymalizowac improty
 import os
 import time
 from copy import copy
-
 import numpy as np
 from pylearn2.datasets import DenseDesignMatrix
 from pylearn2.utils.rng import make_np_rng
-
 import utils
 
 
 class TwoDSiftData(DenseDesignMatrix):
     def __init__(self, filenames=[], y_val=[], nogap_type=True, replicate=0, labels=False, shuffle=True,
                  start=None, stop=None, cv=None):
-        # TODO remember filenames as a dictionary with additional informations, like number of examples, etc.
+        # TODO remember filenames as a dictionary with additional information, like number of examples, etc.
         """
-
         :type filenames: list of data files to read
         :type y_val: list of output values associated with each of the files
         :type nogap_type: True if data written with no spaces between numbers
@@ -27,9 +25,15 @@ class TwoDSiftData(DenseDesignMatrix):
         :type stop: last example
         :type cv: list of CV parts [how many parts, [list of parts to use]]; default None
         """
-        # print ">>>>>>>>>> TwoSiftData.__init__(filenames =", filenames, ", y_val =", y_val, ", replicate =", replicate, \
-        # ", cv =", cv, ")"
         self.filenames = filenames
+        if not os.path.isfile(self.filenames[0]):
+            raise ValueError("Non-existent 2DSIFt file " + self.filenames[0])
+        # get the protein name
+        with open(self.filenames[0]) as f:
+            line = f.readline()
+
+        self.protein = line.split(":")[0]
+
         self.name = ""
         self.__residue_width = 9
         self.__residue_height = 6
@@ -37,139 +41,111 @@ class TwoDSiftData(DenseDesignMatrix):
         self.receptors = []
         self.ligands = []
         self.start_residues = []
-        self.protein = ""
         self.replicate = replicate
-        if not os.path.isfile(self.filenames[0]):
-            raise ValueError("Non-existent 2DSIFt file " + self.filenames[0])
-        # get the protein name
-        f = open(self.filenames[0])
-        line = f.readline()
-        f.close()
-        self.protein = line.split(":")[0]
         self.name = self.protein
         # TODO perhaps window width and height should be parameters?
         self.__win_width = 0
         self.__win_height = 0
+        self.batch_size = 0
         self.examples = 0
         self.cv = cv
         self.splits = []
         self.n_classes = 0
         self.remove_examples = False
         self.add_examples = not self.remove_examples
+
         if nogap_type:
             # TODO add a skipped attribute to be returned
             topo_view, y, skipped = self.read_nogaps(filenames, y_val)
         else:
-            topo_view = self.read()
-        # print len([item for sublist in skipped for item in sublist]), "examples skipped:", skipped
+            # topo_view = self.read()
+            print 'read() function for gap_type files is deprecated, please write a new one to use this file' \
+                  'or use a file with no gaps'
+            return
         # TODO check the assert together with number of skipped records
-        # assert self.examples == len(self.ligands), self.__class__.__name__ + ": number of examples (" + str(self.examples) + \
-        # ") returned not equal to number of ligand keys (" + str(len(self.ligands)) + ")!"
-        axes = ('b', 0, 1, 'c')
+
         # TODO labels are null for the time being
         self.n_classes = len(set(y))
+
         if y is not None:
             y = np.array(y).reshape((self.examples, 1))
         else:
             # TODO tu liczba generowanych etykiet jest ustalona na 2, a powinna byc taka jak w parametrach
             y = np.random.randint(0, 2, (self.examples, 1))
-        if shuffle:
-            self.shuffle_rng = make_np_rng(None, [1, 2, 3], which_method="shuffle")
-            for i in xrange(topo_view.shape[0]):
-                j = self.shuffle_rng.randint(self.examples)
-                # Copy ensures that memory is not aliased.
-                tmp = topo_view[i, :, :, :].copy()
-                topo_view[i, :, :, :] = topo_view[j, :, :, :]
-                topo_view[j, :, :, :] = tmp
 
-                tmp = y[i:i + 1].copy()
-                y[i] = y[j]
-                y[j] = tmp
+        if shuffle:
+            self.shuffle_data(topo_view, y)
 
         # if cv, then split shuffled data into cv parts, and then rearrange so that the parts to use are first
         # then limit the data to the part to be used
         # TODO raise(exception) in case of an error in parameters
         # TODO self.ligands should also be somehow restricted according to cv parameters
-        if cv is not None and isinstance(cv, list):
-            if len(cv) != 2:
-                print "cv split: incorrect cv parameter list:", cv
-            elif not isinstance(cv[0], int) or cv[0] < 2:
-                print "cv split: incorrect number of parts to split:", cv[0]
-            # elif not isinstance(cv[1], list) or len(cv[1]) < 1 or len(cv[1]) > cv[0] - 1:
-            #     if len(cv[1]) == 0:
-            #         self.splits = [int((self.examples / float(cv[0])) * i) for i in range(cv[0])]
-            #     print "cv split: incorrect list of parts to choose from: ", cv[1], "( cv[0] =", cv[0], ")"
-            elif len(cv[1]) == 1 and (cv[1][0] < 0 or cv[1][0] >= cv[0]):
-                print "cv split: incorrect split to choose cv[1] =", cv[1][
-                    0], "in relation to number of splits cv[0] =", \
-                    cv[0]
-            elif len(cv[1]) == cv[0] - 1 and any([item < 0 or item >= cv[0] for item in cv[1]]):
-                print "cv split: incorrect splits to choose cv[1] =", cv[1], "in relation to number of splits cv[0] =", \
-                    cv[0]
-            elif len(cv[1]) == cv[0] - 1 and len(cv[1]) > len(set(cv[1])):
-                print "cv split: incorrect splits to choose, probably duplicates:", cv[1]
+        if cv is not None and isinstance(cv, list) and self.is_cv_valid(self.cv):
+            # compute the possible splits
+            self.splits, remove_ans, add_ans = utils.splits(self.examples, cv[0])
+            # check the splits returned and perform some additional example removal / addition needed
+            if remove_ans[1] == self.examples:
+                self.batch_size = remove_ans[0]
             else:
-                # compute the possible splits
-                self.splits, remove_ans, add_ans = utils.splits(self.examples, cv[0])
-                # check the splits returned and perform some additional example removal / addition needed
-                if remove_ans[1] == self.examples:
+                # addition/removal of examples is needed to have a good batch size
+                if self.remove_examples:
+                    # cut out the last examples, provided they were shuffle earlier
+                    # TODO add some option if examples are not shuffled
+                    topo_view = topo_view[:remove_ans[1], :, :, :]
+                    y = y[:remove_ans[1]]
+                    self.splits = remove_ans[2]
                     self.batch_size = remove_ans[0]
+                    self.examples = remove_ans[1]
+                elif self.add_examples:
+                    topo_view = np.vstack((topo_view, topo_view[add_ans[3], :, :, :]))
+                    y = np.vstack((y, y[add_ans[3]]))
+                    self.splits = add_ans[2]
+                    self.batch_size = add_ans[0]
+                    self.examples = add_ans[1]
                 else:
-                    # addition/removal of examples is needed to have a good batch size
-                    if self.remove_examples:
-                        # cut out the last examples, provided they were shuffle earlier
-                        # TODO add some option if examples are not shuffled
-                        topo_view = topo_view[:remove_ans[1], :, :, :]
-                        y = y[:remove_ans[1]]
-                        self.splits = remove_ans[2]
-                        self.batch_size = remove_ans[0]
-                        self.examples = remove_ans[1]
-                    elif self.add_examples:
-                        topo_view = np.vstack((topo_view, topo_view[add_ans[3], :, :, :]))
-                        y = np.vstack((y, y[add_ans[3]]))
-                        self.splits = add_ans[2]
-                        self.batch_size = add_ans[0]
-                        self.examples = add_ans[1]
-                    else:
-                        raise ValueError("self.remove_examples / self.add_examples not set")
-                if len(cv[1]) == 1:
-                    if cv[1][0] == cv[0] - 1:
-                        from_example = self.splits[-1]
-                        first_not_example = self.examples
-                    elif cv[1][0] == 0:
-                        from_example = 0
-                        first_not_example = self.splits[1]
-                    else:
-                        from_example = self.splits[cv[1][0]]
-                        first_not_example = self.splits[cv[1][0] + 1]
+                    raise ValueError("self.remove_examples / self.add_examples not set")
+
+            if len(cv[1]) == 1:
+                if cv[1][0] == cv[0] - 1:
+                    from_example = self.splits[-1]
+                    first_not_example = self.examples
+                elif cv[1][0] == 0:
+                    from_example = 0
+                    first_not_example = self.splits[1]
+                else:
+                    from_example = self.splits[cv[1][0]]
+                    first_not_example = self.splits[cv[1][0] + 1]
+                topo_view = topo_view[from_example:first_not_example, :, :, :]
+                y = y[from_example:first_not_example]
+            else:
+                cv[1] = sorted(cv[1])
+                missing = list(set(range(cv[0])) - set(cv[1]))[0]
+                if missing == cv[0] - 1:
+                    from_example = 0
+                    first_not_example = self.splits[-1]
+                    topo_view = topo_view[from_example:first_not_example, :, :, :]
+                    y = y[from_example:first_not_example]
+                elif missing == 0:
+                    from_example = self.splits[1]
+                    first_not_example = self.examples
                     topo_view = topo_view[from_example:first_not_example, :, :, :]
                     y = y[from_example:first_not_example]
                 else:
-                    cv[1] = sorted(cv[1])
-                    missing = list(set(range(cv[0])) - set(cv[1]))[0]
-                    if missing == cv[0] - 1:
-                        from_example = 0
-                        first_not_example = self.splits[-1]
-                        topo_view = topo_view[from_example:first_not_example, :, :, :]
-                        y = y[from_example:first_not_example]
-                    elif missing == 0:
-                        from_example = self.splits[1]
-                        first_not_example = self.examples
-                        topo_view = topo_view[from_example:first_not_example, :, :, :]
-                        y = y[from_example:first_not_example]
-                    else:
-                        from_example_first = 0
-                        first_not_example_first = self.splits[missing]
-                        from_example_second = self.splits[missing + 1]
-                        first_not_example_second = self.examples
-                        topo_view = np.vstack((topo_view[from_example_first:first_not_example_first, :, :, :],
-                                               topo_view[from_example_second:first_not_example_second, :, :, :]))
-                        y = np.vstack((y[from_example_first:first_not_example_first],
-                                       y[from_example_second:first_not_example_second]))
-                start = 0
-                stop = topo_view.shape[0]
-                self.examples = stop - start
-        super(TwoDSiftData, self).__init__(topo_view=topo_view, y=y, axes=axes, y_labels=self.n_classes)
+                    from_example_first = 0
+                    first_not_example_first = self.splits[missing]
+                    from_example_second = self.splits[missing + 1]
+                    first_not_example_second = self.examples
+                    topo_view = np.vstack((topo_view[from_example_first:first_not_example_first, :, :, :],
+                                           topo_view[from_example_second:first_not_example_second, :, :, :]))
+                    y = np.vstack((y[from_example_first:first_not_example_first],
+                                   y[from_example_second:first_not_example_second]))
+
+            start = 0
+            stop = topo_view.shape[0]
+            self.examples = stop - start
+        # /end cv part
+        
+        super(TwoDSiftData, self).__init__(topo_view=topo_view, y=y, axes=('b', 0, 1, 'c'), y_labels=self.n_classes)
         assert not np.any(np.isnan(self.X))
 
         if start is not None:
@@ -190,9 +166,7 @@ class TwoDSiftData(DenseDesignMatrix):
             assert self.y.shape[0] == stop - start
 
         self.X = self.X / np.abs(self.X).max()
-        # print ">>>>>>>>>> TwoSiftData.__init__(filenames =", filenames, ", y_val =", y_val, ", replicate =", replicate, \
-        # ", cv =", cv, ")"
-        # print self.__str__()
+
 
     def __str__(self):
         descr = "2D SiFT file:  " + self.name + \
@@ -406,3 +380,38 @@ class TwoDSiftData(DenseDesignMatrix):
                                       )
         return examples, topo_view
 
+    def is_cv_valid(self, cv):
+        if len(cv) != 2:
+                print "cv split: incorrect cv parameter list:", cv
+                return False
+        if not isinstance(cv[0], int) or cv[0] < 2:
+            print "cv split: incorrect number of parts to split:", cv[0]
+            return False
+        if len(cv[1]) == 1 and (cv[1][0] < 0 or cv[1][0] >= cv[0]):
+            print "cv split: incorrect split to choose cv[1] =", cv[1][
+                0], "in relation to number of splits cv[0] =", \
+                cv[0]
+            return False
+        if len(cv[1]) == cv[0] - 1 and any([item < 0 or item >= cv[0] for item in cv[1]]):
+            print "cv split: incorrect splits to choose cv[1] =", cv[1], "in relation to number of splits cv[0] =", \
+                cv[0]
+            return False
+        if len(cv[1]) == cv[0] - 1 and len(cv[1]) > len(set(cv[1])):
+            print "cv split: incorrect splits to choose, probably duplicates:", cv[1]
+            return False
+        return True
+
+    def shuffle_data(self, topo_view, y):
+        self.shuffle_rng = make_np_rng(None, [1, 2, 3], which_method="shuffle")
+        for i in xrange(topo_view.shape[0]):
+            j = self.shuffle_rng.randint(self.examples)
+            # Copy ensures that memory is not aliased.
+            tmp = topo_view[i, :, :, :].copy()
+            topo_view[i, :, :, :] = topo_view[j, :, :, :]
+            topo_view[j, :, :, :] = tmp
+
+            tmp = y[i:i + 1].copy()
+            y[i] = y[j]
+            y[j] = tmp
+
+    # TODO: czy shuffle_data i is_cv_valid dzialaja w miejscu, czy trzeba cos zwracac? grzeczniej byloby zwracac...

ccac65faa18ba0e4231fc02f7e49e811ad69d627
Agnieszka Pocha <agnieszka.pocha@uj.edu.pl>
2015-07-06 19:40:52 +0200

    cleaning

--- src/maxplot.m
+++
@@ -1,5 +0,0 @@
-x = [-100:3:100];
-y = [-100:3:100];
-[xx, yy] = meshgrid(x, y);
-zz = max(xx, yy);
-surf(xx, yy, zz)
--- src/qnormal.m
+++
@@ -1,8 +0,0 @@
-x = [0:0,05:20];
-
-
-for i = 1:2:10
-    y = i * round(x/i);
-    hold on
-    plot(x, y)
-end

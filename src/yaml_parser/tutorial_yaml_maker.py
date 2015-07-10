f68cec3a2654e0e22f8f17bf33862d263fc6c515
Agnieszka Pocha <agnieszka.pocha@uj.edu.pl>
2015-07-10 19:43:00 +0200

    updating yaml_parser/tutorial_yaml_maker.py so it matches the new example.yaml

--- src/yaml_parser/tutorial_yaml_maker.py
+++ src/yaml_parser/tutorial_yaml_maker.py
@@ -9,11 +9,13 @@ import configuration.model as config
 from utils.common import get_timestamp
 import models
 import yaml_parser as yp
+from utils.common import get_timestamp
 
+current_time = get_timestamp()
 
 # setting parameters of convolutional layer
 con = models.ConvElemwise()
-con.layer_name = "con_layer"
+con.layer_name = "h0"
 con.output_channels = 1
 con.kernel_shape = [2, 2]
 con.nonlinearity = models.TanhConvNonlinearity()
@@ -23,7 +25,7 @@ con.pool_shape = [2, 3]
 # setting parameters of softmax layer
 sof = models.Softmax()
 sof.n_classes = 10
-sof.layer_name = "softmax_layer"
+sof.layer_name = "softmax"
 sof.irange = 0.1
 
 # creating list of layers
@@ -40,8 +42,18 @@ mlp = models.MLP()
 mlp.layers = layers
 mlp.input_space = some_space
 
-# creating a dictionary with hyperparameters
-hyper_params = {'model': yp.parse_to_yaml(mlp)}
+# define weight decay parameters. They depend on the number of layers (there is one parameter fo each layer)
+weight_decay_coeffs = yp.parse_weight_decay(mlp)
+
+# define data paths
+path = config.data_path
+
+# generate a filename to store the best model
+pkl_filename = join(config.path_for_storing, current_time+"example_best.pkl")
+
+# create dictionary with hyper parameters
+hyper_params = {'model': yp.parse_to_yaml(mlp), 'path': yp.parse_to_yaml(path),
+                'weight_decay_coeffs': weight_decay_coeffs, 'pkl_filename': pkl_filename}
 
 # obtaining the yaml skelton
 with open(config.yaml_skelton_path) as f:
@@ -50,7 +62,7 @@ with open(config.yaml_skelton_path) as f:
 # filling the yaml skelton with hyperparameters
 yaml_string = default_string % hyper_params
 
-generated_yaml_path = join(config.path_for_storing, get_timestamp+'generated_yaml.yaml')
+generated_yaml_path = join(config.path_for_storing, current_time+'generated_yaml.yaml')
 with open(generated_yaml_path, 'w') as g:
     g.write(yaml_string)
 

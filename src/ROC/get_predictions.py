b516d5a94fc1dbf49101c0842e08fbbd991029be
Agnieszka Pocha <agnieszka.pocha@uj.edu.pl>
2015-07-14 20:39:01 +0200

    some debugging

--- src/ROC/get_predictions.py
+++ src/ROC/get_predictions.py
@@ -6,11 +6,11 @@ import theano
 class Predictor(object):
 
     def __init__(self, model_path):
-        model = serial.load(model_path)
-        X = model.get_input_space().make_theano_batch()
-        Y = model.fprop(X)
-        Y = T.argmax(Y, axis=1)
-        self.f = theano.function([X], Y)
+        model = serial.load(model_path) # some pkl
+        self.X = model.get_input_space().make_theano_batch()
+        self.Y = model.fprop(self.X)
+        self.Y = T.argmax(self.Y, axis=1)
+        self.f = theano.function([self.X], self.Y)
 
     def return_prediction(self, x_test):
         y = self.f(x_test)

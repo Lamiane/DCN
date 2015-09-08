90be50b15a830fe939ab8edffbf8c22cbe5b37c6
Agnieszka Pocha <agnieszka.pocha@uj.edu.pl>
2015-09-08 23:24:56 +0200

    2thres square implemtniondnf

--- src/algorithm_extensions/two_threshold.py
+++ src/algorithm_extensions/two_threshold.py
@@ -24,7 +24,7 @@ class TwoThresholdWRTF1Score(F1Score):
         valid_x = algorithm.monitoring_dataset['valid'].X
         valid_y = algorithm.monitoring_dataset['valid'].y
         predictions = self.predictor.get_predictions(valid_x)
-        upper_threshold, lower_threshold, score = self.compute_optimal_threshold_and_score(valid_y, predictions)
+        lower_threshold, upper_threshold, score = self.compute_optimal_threshold_and_score(valid_y, predictions)
 
         self.thresholds_list.append((upper_threshold, lower_threshold))
         self.score_list.append(score)
@@ -38,69 +38,62 @@ class TwoThresholdWRTF1Score(F1Score):
 
     @staticmethod
     def compute_optimal_threshold_and_score(true_y, predictions):
-        # F1_score = 2TP/(2TP + FN + FP)
-        # our F1_score to deal with not classifying some examples:
-        # if some example isn't classified (is between thresholds) it counts as 1/2 of classifying it wrongly
-        # therefore the F1_score becomes: 2TP/(2TP + FN +FP + 0.5 * unclassified)
-        from numpy import argmax, mean
-        from utils import values
-        import sys
+        axis = sorted([[pred[0][1], label] for pred, label in zip(predictions, true_y)])
 
-        # active     1    [[ 0. 1. 0. ]]    [[ 0. 1. ]]
-        # nonactive  0    [[ 1. 0. 0. ]]    [[ 1. 0. ]]
-        # middle    -1    [[ 0. 0. 1. ]]
+        x = len(axis)
+        y = 0
 
-        dic = {}
-        for pred, tr_y in zip(predictions, true_y):
-            dic[pred[0][1]] = tr_y
+        while y < x and axis[y][1] == 0:
+            y += 1
 
-        # calculating down threshold influence
-        sorted_dic_keys = sorted(dic)   # sorting from lowest to highest
-        score = len(sorted_dic_keys)*0.5    # they're all middle for now
-        min_score = sys.maxint
-        minimums = {}
-        current_minimum_key = 'this value should be overwritten before entering the dictionary'
-        mid_fn_dic = {}
-        num_middle = len(sorted_dic_keys)
-        num_fn = 0
-        # mid_fn_dic[0] = (num_middle, num_fn)
-        for key in sorted_dic_keys:
-            score -= 0.5        # another sample is classified so one middle less
-            num_middle -= 1
-            if dic[key] == 1:   # if it was active, then it will be classified wrongly (false negative)
-                score += 1
-                num_fn += 1
-            if score <= min_score:
-                min_score = score
-                current_minimum_key = key
-            minimums[key] = current_minimum_key     # storing key on which we had minimum
-            mid_fn_dic[key] = (num_middle, num_fn)
+        while x > y and axis[x-1][1] == 1:
+            x -= 1
 
-        num_tp = 0
-        num_fp = 0
-        up_scores = {}
-        for key in reversed(sorted_dic_keys):
-            if dic[key] == 1:   # it was active, we will classify it as active, num_tp grows
-                num_tp += 1
+        tn_global = y
+        tp_global = len(axis) - x
+        fn_global = 0
+        fp_global = 0
+
+        maximal_score = -1.0
+        best_y_idx = y
+        best_x_idx = x
+
+        for i in xrange(y, x+1):
+            tp_local = tp_global
+            fp_local = fp_global
+
+            for j in xrange(x, i-1, -1):
+                middle = len(axis) - tp_local - fp_local - tn_global - fn_global
+                if tp_local == 0:
+                    score_local = 0
+                else:
+                    score_local = 2.0 * tp_local / (2 * tp_local + fp_local + fn_global + 0.5 * middle)
+
+                if score_local > maximal_score:
+                    maximal_score = score_local
+                    best_y_idx, best_x_idx = i, j
+
+                if axis[j-1][1] == 1:
+                    tp_local += 1
+                else:
+                    fp_local += 1
+
+            if i == x:
+                break
+
+            if axis[i][1] == 0:
+                tn_global += 1
             else:
-                num_fp += 1
-            best_down_threshold_key = minimums[key]
-            best_down_threshold_num_mid, best_down_threshold_num_fn = mid_fn_dic[best_down_threshold_key]
-            num_middle = best_down_threshold_num_mid - num_tp - num_fp  # some examples are now classified
-            if num_middle < 0:
-                continue    # breaking if thresholds swap
-            up_score = (2 * float(num_tp))/(2 * float(num_tp) + num_fp + best_down_threshold_num_fn + 0.5 * float(num_middle))
-            print 'key:', key
-            print 'lower:', best_down_threshold_key, 'upper:', key
-            print 'tp:', num_tp, '\nfp:', num_fp, '\tfn:', best_down_threshold_num_fn
-            print 'middle', num_middle
-            print 'middle from lower:', best_down_threshold_num_mid
-            print 'score:', up_score, '\n'
-            up_scores[key] = up_score
+                fn_global += 1
 
-        key_with_maximal_up_threshold_score = max(up_scores, key=up_scores.get)
-        maximal_score = up_scores[key_with_maximal_up_threshold_score]
-        respective_down_threshold = minimums[key_with_maximal_up_threshold_score] + sys.float_info.min
-        key_with_maximal_up_threshold_score -= sys.float_info.min
+        print best_y_idx, best_x_idx
 
-        return key_with_maximal_up_threshold_score, respective_down_threshold, maximal_score
+        best_y = 0 if best_y_idx == 0 else 1
+        if len(axis) > best_y_idx > 0:
+            best_y = (axis[best_y_idx-1][0] + axis[best_y_idx][0]) / 2.0
+
+        best_x = 0 if best_x_idx == 0 else 1
+        if len(axis) > best_x_idx > 0:
+            best_x = (axis[best_x_idx-1][0] + axis[best_x_idx][0]) / 2.0
+
+        return best_y, best_x, maximal_score
